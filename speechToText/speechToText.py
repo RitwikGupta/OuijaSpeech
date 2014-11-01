26aed3b18ba769e86ac5485c8962e02c278633d2
Rajat Mehndiratta <rajatmehndiratta@cmu.edu>
2014-10-31 23:04:25 -0500

    speech to text

---
+++ speechToText/speechToText.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+import time
+r = sr.Recognizer()
+
+start = end = time.time()
+# while True:
+with sr.Microphone() as source:
+    r.energy_threshold = 100
+    audio = r.listen(source, timeout = .5)
+
+end = time.time()
+
+# print "Took", (end-start), "seconds"
+
+start = end = time.time()
+    
+try:
+    print r.recognize(audio)
+
+except LookupError:
+    print "-1"
