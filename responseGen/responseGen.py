4f02b52faa3ae651cbf11c0d9f680b1c6627d76b
Rajat Mehndiratta <rajatmehndiratta@cmu.edu>
2014-11-02 02:03:26 -0600

    Yo

--- responseGen/responseGen.py
+++ responseGen/responseGen.py
@@ -9,6 +9,7 @@ from functools import wraps
 import errno
 import os
 import signal
+import requests
 from timeout import timeout
 
 """class TimeoutError(Exception):
@@ -34,9 +35,13 @@ def timeout(seconds=7, error_message=os.strerror(errno.ETIME)):
 
 def mainFunction():
     qu = sys.argv[1]
+    user = "Yolo!"
 
     if(qu == "-1"):
-        response = "&"
+        response = "&+"
+
+    elif(qu == "-2"):
+        response = "+"
 
     else:
         cb1 = cleverbot.Cleverbot()
@@ -50,9 +55,40 @@ def mainFunction():
         while (response[0] == "*" or response.find("app") != -1 or response.find("clever") != -1 or response.find("bot") != -1 or response.find("com") != -1):
             response = cb1.ask(qu)
 
+        respon = response
+
         #Make all spaces periods
         response = response.replace(" ", ".")
         response = response.lower() + "&"
+
+    username = sys.argv[2]
+
+    if username == None:
+        pass
+
+    elif "0710106541" not in username and reponse != "&":
+        if response = "+&":
+            respo = "Yo"
+        else:
+            respo = respon
+        user = username
+        conversation = str(user) + "said: " + str(qu) + " <br>" + "Bot responded: " + str(respo) + " <br>"
+        requestURL = "http://www.purduecs.com/write.php?username=" + str(user) + "&conversation=" + str(conversation)
+        r = requests.get(requestURL)
+
+    else:
+        if response = "+&":
+            respo = "Yo"
+        elif response = "&":
+            response = "Farewell, comrade!"
+        else:
+            respo = respon
+        user = username.replace("0710106541", "")
+        conversation = str(user) + "said: " + str(qu) + " <br>" + "Bot responded: " + str(respo) + " <br>"
+        requestURL = "http://www.purduecs.com/write.php?username=" + str(user) + "&conversation=" + str(conversation)
+        r = requests.get(requestURL)
+        readURL = "http://www.purduecs.com/read.php?username=" + str(user)
+        requests.post("http://api.justyo.co/yo/", data={'api_token': "ec7efb15-f6e3-4548-91f3-81daf06446b2", 'username': user, 'link': readURL})
 
     return response
 
--- speechToText/speechToText.py
+++ speechToText/speechToText.py
@@ -4,6 +4,7 @@ from basicAnimationClass import BasicAnimationClass
 import time
 import math
 import subprocess
+import requests
 
 class speechToText(BasicAnimationClass):
     def __init__(self, width=800, height=600):
@@ -15,20 +16,60 @@ class speechToText(BasicAnimationClass):
         super(speechToText, self).__init__(canvasWidth, canvasHeight)
 
     def redrawAll(self):
+        outlineColor = "white"
         cx, cy, r = self.cx, self.cy, self.radius
+        userString = ""
         self.canvas.delete(ALL)
+        if self.username != None:
+            username = (self.username).replace("0710106541", "")
+        else:
+            username = None
+        self.canvas.create_text(cx, cy/3, text="OuijaBot", fill="white", font="Arial 30 bold")
+        if username != None:
+            userString = "Conversation with " + username
+            self.canvas.create_text(cx, self.height - r/2, text=userString, font="Arial 20 bold", fill=outlineColor)
         if self.isRecording:
             outlineColor = "red"
             self.canvas.create_oval(cx-(r/3), cy-(r/3), cx+(r/3), cy+(r/3), fill=outlineColor)
             self.canvas.create_text(cx, cy/2, text="Recording...", fill=outlineColor, font="Arial 20 bold")
             self.canvas.create_text(cx, cy/2 + 23, text="(stops automatically)", fill=outlineColor, font="Arial 15")
-        else:
+        elif self.checkingForYo:
+            outlineColor = "red"
+            self.canvas.create_text(cx, cy, fill=outlineColor, text="Yo", font="Arial 20 bold")
+            self.canvas.create_text(cx, cy/2, text="Checking for Yo's...", fill=outlineColor, font="Arial 20 bold")
+        elif self.receivedAYo == False:
             outlineColor = "white"
             self.canvas.create_text(cx, cy/2, text="Click the button the start recording", fill=outlineColor, font="Arial 20 bold")
             self.canvas.create_oval(cx-(r/3), cy-(r/3), cx+(r/3), cy+(r/3), fill=outlineColor)
 
-        self.canvas.create_oval(cx-r, cy-r, cx+r, cy+r, width=4, fill=None, outline=outlineColor)
+        if self.receivedAYo == False:
+            self.canvas.create_oval(cx-r, cy-r, cx+r, cy+r, width=4, fill=None, outline=outlineColor)
+        else:
+            outlineColor = "white"
+            self.canvas.create_rectangle(cx-r-10, cy-r-10, cx+r+10, cy+r+10, width=4, fill="white")
+            self.canvas.create_rectangle(cx-r, cy-r, cx+r, cy+r, width=4, fill="purple", outline="white")
+            self.canvas.create_text(cx, cy, fill=outlineColor, text="Yo", font="Arial 30 bold")
 
+    def onTimerFired(self):
+        if self.username == None:
+            self.checkingForYo = True
+            self.redrawAll()
+            r = requests.get('http://www.purduecs.com')
+            self.checkingForYo = False
+            if r.text != "No":
+                self.receivedAYo = True
+                if self.receivedAYo == True:
+                    self.username = r.text
+            time.sleep(1)
+            self.redrawAll()
+            try:
+                subprocess.call(["../responseGen/responseGen.py", "-2"])
+            except:
+                pass
+            time.sleep(3)
+            self.receivedAYo = False
+            self.redrawAll()
+    
     def onMousePressed(self, event):
         distance = math.sqrt((event.x - self.cx)**2 + (event.y - self.cy)**2)
         if distance <= self.radius:
@@ -38,6 +79,14 @@ class speechToText(BasicAnimationClass):
         else:
             self.isRecording = False
             self.redrawAll()
+
+    def onKeyPressed(self, event):
+        keysym = event.keysym
+        if keysym == "r" or keysym == "y" or keysym == "o":
+            if "0710106541" in self.username:
+                self.username = None
+            elif self.username != None:
+                self.username = self.username + "0710106541"
 
     def getText(self):
         r = sr.Recognizer()
@@ -49,26 +98,30 @@ class speechToText(BasicAnimationClass):
             self.redrawAll()
 
             try:
-                subprocess.call(["../responseGen/responseGen.py", str(r.recognize(audio, fuckedUp))])
+                try:
+                    if self.username == None:
+                        subprocess.call(["../responseGen/responseGen.py", str(r.recognize(audio, fuckedUp))])
+                    else:
+                        subprocess.call(["../responseGen/responseGen.py", str(r.recognize(audio, fuckedUp)), self.username])
+                except:
+                    pass
 
             except LookupError:
-                subprocess.call(["../responseGen/responseGen.py", "-1"])
+                try:
+                    if self.username == None:
+                        subprocess.call(["../responseGen/responseGen.py", "-1"])
+                    else:
+                        subprocess.call(["../responseGen/responseGen.py", "-1", self.username])
+                except:
+                    pass
 
     def initAnimation(self):
         self.isRecording = False
+        self.app.setTimerDelay(7000) # every seven seconds
+        self.checkingForYo = False
+        self.receivedAYo = False
+        self.username = None
 
 stt = speechToText(800, 600)
 
 stt.run()
-
-"""r = sr.Recognizer()
-
-with sr.Microphone() as source:
-    r.energy_threshold = 100
-    audio = r.listen(source, timeout = 1)
-
-    try:
-        print r.recognize(audio)
-
-    except LookupError:
-        print "-1"""""
